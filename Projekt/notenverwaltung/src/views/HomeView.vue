ac81fc5aa57e83a752812839f30c217bf6c47336
dstuebi <55478193+dstuebi@users.noreply.github.com>
2022-06-22 18:34:51 +0200

    Added Array

--- Projekt/notenverwaltung/src/views/HomeView.vue
+++ Projekt/notenverwaltung/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <table>
+    <tr>
+      <th v-for="subject in subjects"> {{ subject.subject }}</th>
+    </tr>
+    <tr>
+      <td v-for="subject in subjects"> {{ subject.grades }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
@@ -13,6 +17,23 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      subjects: [{
+        "subject": "M152", 
+        "grades": [5, 4]
+      }, {
+        "subject": "M151",
+        "grades": [5, 5.78]
+      }, {
+        "subject": "ABU",
+        "grades": [6]
+      }]
+    }
   }
 }
+
+
+
 </script>
